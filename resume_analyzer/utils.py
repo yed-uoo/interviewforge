f496f0c6ba674072357d6d0a771f328c9fceaed0
yed-uoo <yedu11kr2005@gmail.com>
2026-05-26 12:19:00 +0530

    Add exception handling for parsing error and api error

--- resume_analyzer/utils.py
+++ resume_analyzer/utils.py
@@ -2,15 +2,23 @@ import fitz
 
 
 def extract_text_from_pdf(pdf_path):
-    doc = fitz.open(pdf_path)
-    full_text = []
+    try:
+        doc = fitz.open(pdf_path)
+    
+        full_text = []
 
-    for page in doc:
-        blocks = page.get_text("blocks")
-        blocks = sorted(blocks, key=lambda b: (b[1], b[0]))
+        for page in doc:
+            blocks = page.get_text("blocks")
+            blocks = sorted(blocks, key=lambda b: (b[1], b[0]))
 
-        for block in blocks:
-            full_text.append(block[4])
+            for block in blocks:
+                full_text.append(block[4])
 
-    doc.close()
-    return "\n".join(full_text)
+        doc.close()
+        return "\n".join(full_text)
+    
+    except Exception as e:
+
+        print("PDF PARSING ERROR:", e)
+
+        return None
